b66e1bdfc24b906ba290a836875a3b705a19b67e
testoftiramisu <testoftiramisu@gmail.com>
2016-11-16 16:57:54 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    private static final String LOCK_SCREEN_DEFAULT_LOCATION = "C:\\Windows\\System32\\oobe\\info\\backgrounds\\backgroundDefault.jpg";
+    private static final String INPUT_FILE = "C:\\tmp.jpg";
+
+    private static final String REGISTRY_PATH = "SOFTWARE\\Policies\\Microsoft\\Windows\\Personalization";
+
+    public static void main(String... args) {
+        String newLocation;
+        System.out.println("LockScreenImage = " + lockScreenImageLocation());
+
+        newLocation = args.length > 0 ? args[0] : INPUT_FILE;
+        updateLockScreenImageLocation(newLocation);
+
+        System.out.println("New Lock Screen Image = " + lockScreenImageLocation());
+
+    }
+
+    private static void updateLockScreenImageLocation(String newLocation) {
+
+        try {
+            WinRegistry.writeStringValue(WinRegistry.HKEY_LOCAL_MACHINE, REGISTRY_PATH, "LockScreenImage", newLocation);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static String lockScreenImageLocation() {
+        String value = null;
+        try {
+            value = WinRegistry.readString(WinRegistry.HKEY_LOCAL_MACHINE, REGISTRY_PATH, "LockScreenImage");
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+}
