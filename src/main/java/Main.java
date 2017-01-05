25bb9676d0cd01439e354b393d99c7eedab34608
ТЫДщп <testoftiramisu@gmail.com>
2017-01-05 17:24:15 +0100

    Some improvements for Checkstyle consistency.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,12 @@ import java.lang.reflect.InvocationTargetException;
 public class Main {
     private static final String INPUT_FILE = "C:\\tmp.jpg";
 
-    private static final String REGISTRY_PATH = "SOFTWARE\\Policies\\Microsoft\\Windows\\Personalization";
+    private static final String REGISTRY_PATH =
+            "SOFTWARE\\Policies\\Microsoft\\Windows\\Personalization";
 
+    /**
+     * Application entry point.
+     */
     public static void main(String... args) {
         String newLocation;
         System.out.println("Current Lock Screen image: " + lockScreenImageLocation());
@@ -16,27 +20,27 @@ public class Main {
     }
 
     private static void updateLockScreenImageLocation(String newLocation) {
-
         try {
             System.out.println("Trying to set " + newLocation + " as Lock Screen image...");
-            WinRegistry.writeStringValue(WinRegistry.HKEY_LOCAL_MACHINE, REGISTRY_PATH, "LockScreenImage", newLocation);
-        } catch (IllegalAccessException e) {
-            System.out.println(e.getMessage());
+            WinRegistry.writeStringValue(WinRegistry.HKEY_LOCAL_MACHINE, REGISTRY_PATH,
+                    "LockScreenImage", newLocation);
+        } catch (IllegalAccessException ex) {
+            System.out.println(ex.getMessage());
             System.exit(1);
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
+        } catch (InvocationTargetException ex) {
+            ex.printStackTrace();
         }
-
     }
 
     private static String lockScreenImageLocation() {
         String value = null;
         try {
-            value = WinRegistry.readString(WinRegistry.HKEY_LOCAL_MACHINE, REGISTRY_PATH, "LockScreenImage");
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
+            value = WinRegistry.readString(WinRegistry.HKEY_LOCAL_MACHINE, REGISTRY_PATH,
+                    "LockScreenImage");
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InvocationTargetException ex) {
+            ex.printStackTrace();
         }
         return value;
     }
--- src/main/java/WinRegistry.java
+++ src/main/java/WinRegistry.java
@@ -1,43 +1,44 @@
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.prefs.Preferences;
 
-public class WinRegistry {
+final class WinRegistry {
     private static final int ERROR_CODE = 1;
-    public static final int HKEY_CURRENT_USER = 0x80000001;
-    public static final int HKEY_LOCAL_MACHINE = 0x80000002;
-    public static final int REG_SUCCESS = 0;
-    public static final int REG_NOTFOUND = 2;
-    public static final int REG_ACCESSDENIED = 5;
-
+    private static final int HKEY_CURRENT_USER = 0x80000001;
+    static final int HKEY_LOCAL_MACHINE = 0x80000002;
+    private static final int REG_SUCCESS = 0;
+    private static final int REG_NOTFOUND = 2;
+    private static final int REG_ACCESSDENIED = 5;
     private static final int KEY_ALL_ACCESS = 0xf003f;
     private static final int KEY_READ = 0x20019;
-    private static final Preferences userRoot = Preferences.userRoot();
-    private static final Preferences systemRoot = Preferences.systemRoot();
-    private static final Class<? extends Preferences> userClass = userRoot.getClass();
-    private static final Method regOpenKey;
-    private static final Method regCloseKey;
-    private static final Method regQueryValueEx;
-    private static final Method regSetValueEx;
+    private static final Preferences USER_ROOT = Preferences.userRoot();
+    private static final Preferences SYSTEM_ROOT = Preferences.systemRoot();
+    private static final Class<? extends Preferences> USER_CLASS = USER_ROOT.getClass();
+    private static final Method REG_OPEN_KEY;
+    private static final Method REG_CLOSE_KEY;
+    private static final Method REG_QUERY_VALUE_EX;
+    private static final Method REG_SET_VALUE_EX;
+    private static final Method REG_CREATE_KEY_EX;
 
     static {
         try {
-            regOpenKey = userClass.getDeclaredMethod("WindowsRegOpenKey", int.class, byte[].class, int.class);
-            regOpenKey.setAccessible(true);
+            REG_OPEN_KEY = USER_CLASS.getDeclaredMethod("WindowsRegOpenKey", int.class, byte[].class, int.class);
+            REG_OPEN_KEY.setAccessible(true);
 
-            regCloseKey = userClass.getDeclaredMethod("WindowsRegCloseKey", int.class);
-            regCloseKey.setAccessible(true);
+            REG_CLOSE_KEY = USER_CLASS.getDeclaredMethod("WindowsRegCloseKey", int.class);
+            REG_CLOSE_KEY.setAccessible(true);
 
-            regQueryValueEx = userClass.getDeclaredMethod("WindowsRegQueryValueEx", int.class, byte[].class);
-            regQueryValueEx.setAccessible(true);
+            REG_QUERY_VALUE_EX = USER_CLASS.getDeclaredMethod("WindowsRegQueryValueEx", int.class, byte[].class);
+            REG_QUERY_VALUE_EX.setAccessible(true);
 
-            regSetValueEx = userClass.getDeclaredMethod("WindowsRegSetValueEx", int.class, byte[].class, byte[].class);
-            regSetValueEx.setAccessible(true);
+            REG_SET_VALUE_EX = USER_CLASS.getDeclaredMethod("WindowsRegSetValueEx", int.class, byte[].class, byte[].class);
+            REG_SET_VALUE_EX.setAccessible(true);
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            REG_CREATE_KEY_EX = USER_CLASS.getDeclaredMethod("WindowsRegCreateKeyEx", int.class, byte[].class);
+            REG_CREATE_KEY_EX.setAccessible(true);
+
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
         }
     }
 
@@ -45,7 +46,7 @@ public class WinRegistry {
     }
 
     /**
-     * Read a value from key and value name
+     * Read a value from key and value name.
      *
      * @param hkey      HKEY_CURRENT_USER/HKEY_LOCAL_MACHINE
      * @param key
@@ -58,16 +59,29 @@ public class WinRegistry {
     public static String readString(int hkey, String key, String valueName)
             throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
         if (hkey == HKEY_LOCAL_MACHINE) {
-            return readString(systemRoot, hkey, key, valueName);
+            return readString(SYSTEM_ROOT, hkey, key, valueName);
         } else if (hkey == HKEY_CURRENT_USER) {
-            return readString(userRoot, hkey, key, valueName);
+            return readString(USER_ROOT, hkey, key, valueName);
         } else {
             throw new IllegalArgumentException("hkey=" + hkey);
         }
     }
 
+    private static String readString(Preferences root, int hkey, String key, String value)
+            throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+        int[] handles = (int[]) REG_OPEN_KEY.invoke(root, hkey, stringToByteArray(key), KEY_READ);
+
+        if (handles[ERROR_CODE] != REG_SUCCESS) {
+            return null;
+        }
+
+        byte[] valb = (byte[]) REG_QUERY_VALUE_EX.invoke(root, handles[0], stringToByteArray(value));
+        REG_CLOSE_KEY.invoke(root, handles[0]);
+        return (valb != null ? new String(valb).trim() : null);
+    }
+
     /**
-     * Write a value in a given key/value name
+     * Write a value in a given key/value name.
      *
      * @param hkey
      * @param key
@@ -77,47 +91,40 @@ public class WinRegistry {
      * @throws IllegalAccessException
      * @throws InvocationTargetException
      */
-    public static void writeStringValue(int hkey, String key, String valueName, String value)
+    static void writeStringValue(int hkey, String key, String valueName, String value)
             throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
         if (hkey == HKEY_LOCAL_MACHINE) {
-            writeStringValue(systemRoot, hkey, key, valueName, value);
+            writeStringValue(SYSTEM_ROOT, hkey, key, valueName, value);
         } else if (hkey == HKEY_CURRENT_USER) {
-            writeStringValue(userRoot, hkey, key, valueName, value);
+            writeStringValue(USER_ROOT, hkey, key, valueName, value);
         } else {
             throw new IllegalArgumentException("hkey=" + hkey);
         }
     }
 
-    private static String readString(Preferences root, int hkey, String key, String value)
-            throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
-        int[] handles = (int[]) regOpenKey.invoke(root, hkey, stringToByteArray(key), KEY_READ);
-
-        if (handles[ERROR_CODE] != REG_SUCCESS) {
-            return null;
-        }
-
-        byte[] valb = (byte[]) regQueryValueEx.invoke(root, handles[0], stringToByteArray(value));
-        regCloseKey.invoke(root, handles[0]);
-        return (valb != null ? new String(valb).trim() : null);
-    }
 
     private static void writeStringValue(Preferences root, int hkey, String key, String valueName, String value)
             throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
-        int[] handles = (int[]) regOpenKey.invoke(root, hkey, stringToByteArray(key), KEY_ALL_ACCESS);
+        int[] handles = (int[]) REG_OPEN_KEY.invoke(root, hkey, stringToByteArray(key), KEY_ALL_ACCESS);
         int rc;
 
         if (handles[ERROR_CODE] == REG_ACCESSDENIED) {
-            throw new IllegalAccessException("Key " + key + " cannot be opened. Access denied.\nTry to re-run application with Administrator privileges.");
+            throw new IllegalAccessException("Key " + key + " cannot be opened. Access denied.\n" + "Try to re-run application with Administrator privileges.");
         } else {
-            rc = (Integer) regSetValueEx.invoke(root, handles[0], stringToByteArray(valueName), stringToByteArray(value));
+            rc = (Integer) REG_SET_VALUE_EX.invoke(root, handles[0], stringToByteArray(valueName), stringToByteArray(value));
             if (rc == REG_SUCCESS) {
-                regCloseKey.invoke(root, handles[0]);
+                REG_CLOSE_KEY.invoke(root, handles[0]);
             }
         }
     }
 
+    private static int[] createKey(Preferences root, int hkey, String key)
+            throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+        return (int[]) REG_CREATE_KEY_EX.invoke(root, hkey, stringToByteArray(key));
+    }
+
     /**
-     * * Returns this java string as a null-terminated byte array
+     * Returns this java string as a null-terminated byte array.
      */
     private static byte[] stringToByteArray(String str) {
         byte[] result = new byte[str.length() + 1];
